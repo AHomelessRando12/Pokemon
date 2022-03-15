13a8d9c426f4269bc53d9a7b9053ae06bd1d3b3f
AHomelessRando12 <85993071+AHomelessRando12@users.noreply.github.com>
2022-03-15 13:54:01 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,47 @@ pygame.display.set_caption("Pokemon Showdown!")
 white = (255, 255, 255)
 black = (0, 0, 0)
 
+types = {"normal": 0,
+         "fire": 1,
+         "water": 2,
+         "electric": 3,
+         "grass": 4,
+         "ice": 5,
+         "fighting": 6,
+         "poison": 7,
+         "ground": 8,
+         "flying": 9,
+         "psychic": 10,
+         "bug": 11,
+         "rock": 12,
+         "ghost": 13,
+         "dragon": 14,
+         "dark": 15,
+         "steel": 16,
+         "fairy": 17,
+         "none": 18
+         }
+
+type_effectiveness = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1, 1],
+                      [1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1, 1],
+                      [1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1, 1],
+                      [1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1, 1],
+                      [1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1, 1],
+                      [1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1, 1],
+                      [2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5, 1],
+                      [1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2, 1],
+                      [1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1, 1],
+                      [1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1, 1],
+                      [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1, 1],
+                      [1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5, 1],
+                      [1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1, 1],
+                      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1, 1],
+                      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0, 1],
+                      [1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5, 1],
+                      [1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2, 1],
+                      [1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1, 1],
+                      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
 pokemonId = input("Enter the id of the pokemon you want to use: ")
 pokemonLevel = int(input("Enter the level of the pokemon: "))
 enemyLevel = int(input("Enter the level of pokemon you want to fight: "))
@@ -31,18 +72,17 @@ with open("pokemondata.txt", "r") as file:
 pokeList.append(pokemonLevel)
 
 baseHP = float(pokeList[4])
-pokeList[4] = round((((2 * baseHP * pokemonLevel) / 100) + pokemonLevel + 10))
 baseAtk = float(pokeList[5])
-pokeList[5] = round((((2 * baseAtk) * pokemonLevel) / 100) + 5)
 baseDef = float(pokeList[6])
-pokeList[6] = round((((2 * baseDef) * pokemonLevel) / 100) + 5)
 baseSpAtk = float(pokeList[7])
-pokeList[7] = round((((2 * baseSpAtk) * pokemonLevel) / 100) + 5)
 baseSpDef = float(pokeList[8])
-pokeList[8] = round((((2 * baseSpDef) * pokemonLevel) / 100) + 5)
 baseSpe = float(pokeList[9])
-pokeList[9] = round((((2 * baseSpe) * pokemonLevel) / 100) + 5)
-
+pokeList[4] = round(((2 * baseHP * pokemonLevel) / 100) + pokemonLevel + 10)
+pokeList[5] = round(((2 * baseAtk * pokemonLevel) / 100) + 5)
+pokeList[6] = round(((2 * baseDef * pokemonLevel) / 100) + 5)
+pokeList[7] = round(((2 * baseSpAtk * pokemonLevel) / 100) + 5)
+pokeList[8] = round(((2 * baseSpDef * pokemonLevel) / 100) + 5)
+pokeList[9] = round(((2 * baseSpe * pokemonLevel) / 100) + 5)
 
 randomEnemy = str(random.randint(1, 9))
 
@@ -60,104 +100,50 @@ with open("pokemondata.txt", "r") as file:
                 for j in range(len(lineList[i])):
                     pokeList2.append(lineList[i][j])
 
+baseHPEnemy = float(pokeList2[4])
+baseAtkEnemy = float(pokeList2[5])
+baseDefEnemy = float(pokeList2[6])
+baseSpAtkEnemy = float(pokeList2[7])
+baseSpDefEnemy = float(pokeList2[8])
+baseSpeEnemy = float(pokeList2[9])
+pokeList2[4] = round(((2 * baseHPEnemy * enemyLevel) / 100) + enemyLevel + 10)
+pokeList2[5] = round(((2 * baseAtkEnemy * enemyLevel) / 100) + 5)
+pokeList2[6] = round(((2 * baseDefEnemy * enemyLevel) / 100) + 5)
+pokeList2[7] = round(((2 * baseSpAtkEnemy * enemyLevel) / 100) + 5)
+pokeList2[8] = round(((2 * baseSpDefEnemy * enemyLevel) / 100) + 5)
+pokeList2[9] = round(((2 * baseSpeEnemy * enemyLevel) / 100) + 5)
+
 print(pokeList)
 print(pokeList2)
 
 
 class Move:
-    def __init__(self, name, type, contact, pp, power, accuracy, status, effect, stat, level, attack, defense,
-                 pokeType, allyList, enemyList):
-        self.name = name
-        self.type = type
-        self.contact = contact
-        self.pp = pp
-        self.power = power
-        self.accuracy = accuracy
-        self.status = status
-        self.effect = effect
-        self.stat = stat
-        self.level = level
-        self.attack = attack
-        self.defense = defense
-        self.pokeType = pokeType
+    def __init__(self, allyList, enemyList, ally, enemy):
         self.allyList = allyList
         self.enemyList = enemyList
+        self.ally = ally
+        self.enemy = enemy
 
-    def allyDamage(self, allyList, enemyList):
+    def Damage(self, allyList, enemyList, ally, enemy):
         self.allyList = allyList
         self.enemyList = enemyList
-        moveList = []
-        moveId = input("Enter the id of the move you want to use: ")
-        with open("movedata.txt", "r") as file:
-            line = True
-            lineList = []
-            while line != "":
-                line = file.readline()
-                if line != "":
-                    split = line.split(",")
-                    lineList.append(split)
-                for i in range(len(lineList)):
-                    if lineList[i][0] == moveId:
-                        print("Hello")
-                        for j in range(len(lineList[i])):
-                            moveList.append(lineList[i][j])
-                            print(moveList)
+        self.ally = ally
+        self.enemy = enemy
 
-        types = {"normal": 0,
-                 "fire": 1,
-                 "water": 2,
-                 "electric": 3,
-                 "grass": 4,
-                 "ice": 5,
-                 "fighting": 6,
-                 "poison": 7,
-                 "ground": 8,
-                 "flying": 9,
-                 "psychic": 10,
-                 "bug": 11,
-                 "rock": 12,
-                 "ghost": 13,
-                 "dragon": 14,
-                 "dark": 15,
-                 "steel": 16,
-                 "fairy": 17,
-                 "none": 18
-                 }
+        effectiveness = type_effectiveness[types[ally[2]]][types[enemy[2]]] * \
+                        type_effectiveness[types[ally[2]]][types[enemy[3]]]
 
-        type_effectiveness = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1, 1],
-                              [1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1, 1],
-                              [1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1, 1],
-                              [1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1, 1],
-                              [1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1, 1],
-                              [1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1, 1],
-                              [2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5, 1],
-                              [1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2, 1],
-                              [1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1, 1],
-                              [1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1, 1],
-                              [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1, 1],
-                              [1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5, 1],
-                              [1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1, 1],
-                              [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1, 1],
-                              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0, 1],
-                              [1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5, 1],
-                              [1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2, 1],
-                              [1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1, 1],
-                              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-
-        effectiveness = type_effectiveness[types[moveList[2]]][types[pokeList2[2]]] * \
-                        type_effectiveness[types[moveList[2]]][types[pokeList2[3]]]
-
-        if moveList[3] == "physical":
+        if ally[3] == "physical":
             moveDamage = True
             contact = True
             attack = float(allyList[5])
             defense = float(enemyList[6])
-        elif moveList[3] == "special":
+        elif ally[3] == "special":
             moveDamage = True
             contact = False
             attack = float(allyList[7])
             defense = float(enemyList[8])
-        elif moveList[3] == "status":
+        elif ally == "status":
             moveDamage = False
             contact = False
         critical = random.randint(0, 100)
@@ -165,113 +151,57 @@ class Move:
             checkCritical = True
         else:
             checkCritical = 1
-        if allyList[2] or allyList[3] == moveList[2]:
+        if allyList[2] or allyList[3] == ally[2]:
             stab = 1.5
         else:
             stab = 1
-        if moveList[3] != "status":
+        if moveDamage:
             damage = ((((((2 * allyList[13]) / 5) + 2) * float(moveList[5]) * (
                     attack / defense)) / 50) + 2) * checkCritical * (
                              random.randint(85, 100) / 100) * stab * effectiveness  # *burn/frostbite
         else:
             damage = 0
-
         return damage
 
-    def enemyDamage(self, allyList, enemyList):
+    def moveSelection(self, allyList, enemyList):
         self.allyList = allyList
         self.enemyList = enemyList
+
         moveList = []
-        moveId = input("Enter the id of the move you want to use: ")
+        moveList2 = []
+
+        randomMove = random.randint(1, 18)
+        moveSelection = int(input("Enter the ID of the move you want to use: "))
+
         with open("movedata.txt", "r") as file:
             line = True
             lineList = []
+
             while line != "":
                 line = file.readline()
                 if line != "":
                     split = line.split(",")
                     lineList.append(split)
                 for i in range(len(lineList)):
-                    if lineList[i][0] == moveId:
-                        print("Hello")
+                    print(i)
+                    if lineList[i][0] == moveSelection:
                         for j in range(len(lineList[i])):
                             moveList.append(lineList[i][j])
-                            print(moveList)
 
-        types = {"normal": 0,
-                 "fire": 1,
-                 "water": 2,
-                 "electric": 3,
-                 "grass": 4,
-                 "ice": 5,
-                 "fighting": 6,
-                 "poison": 7,
-                 "ground": 8,
-                 "flying": 9,
-                 "psychic": 10,
-                 "bug": 11,
-                 "rock": 12,
-                 "ghost": 13,
-                 "dragon": 14,
-                 "dark": 15,
-                 "steel": 16,
-                 "fairy": 17,
-                 "none": 18
-                 }
+            print(moveList)
+            lineList = []
 
-        type_effectiveness = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1, 1],
-                              [1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1, 1],
-                              [1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1, 1],
-                              [1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1, 1],
-                              [1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1, 1],
-                              [1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1, 1],
-                              [2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5, 1],
-                              [1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2, 1],
-                              [1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1, 1],
-                              [1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1, 1],
-                              [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1, 1],
-                              [1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5, 1],
-                              [1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1, 1],
-                              [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1, 1],
-                              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0, 1],
-                              [1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5, 1],
-                              [1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2, 1],
-                              [1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1, 1],
-                              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+            while line != "":
+                line = file.readline()
+                if line != "":
+                    split = line.split(",")
+                    lineList.append(split)
+                for i in range(len(lineList)):
+                    if lineList[i][0] == randomMove:
+                        for j in range(len(lineList[i])):
+                            moveList2.append(lineList[i][j])
 
-        effectiveness = type_effectiveness[types[moveList[2]]][types[pokeList[2]]] * \
-                        type_effectiveness[types[moveList[2]]][types[pokeList[3]]]
-
-        if moveList[3] == "physical":
-            moveDamage = True
-            contact = True
-            attack = float(enemyList[5])
-            defense = float(allyList[6])
-        elif moveList[3] == "special":
-            moveDamage = True
-            contact = False
-            attack = float(enemyList[7])
-            defense = float(allyList[8])
-        elif moveList[3] == "status":
-            moveDamage = False
-            contact = False
-        critical = random.randint(0, 100)
-        if critical < 7:
-            checkCritical = True
-        else:
-            checkCritical = 1
-        if enemyList[2] or enemyList[3] == moveList[2]:
-            stab = 1.5
-        else:
-            stab = 1
-        if moveList[3] != "status":
-            damage = ((((((2 * enemyList[13]) / 5) + 2) * float(moveList[5]) * (
-                    attack / defense)) / 50) + 2) * checkCritical * (
-                             random.randint(85, 100) / 100) * stab * effectiveness  # *burn/frostbite
-        else:
-            damage = 0
-
-        return damage
+        return moveList, moveList2
 
 
 class Ally:
@@ -291,14 +221,14 @@ class Ally:
         self.damage = damage
 
     def draw(self):
-        fainted = False
         image = pygame.image.load(self.backSprite)
         self.surface.blit(image, (0, 50))
 
-    def health(self):
-        health = self.hitpoints
-        health = int(health) - self.damage
-
+    def health(self, damage):
+        self.damage = damage
+        health = self.hitPoints
+        health = int(health) - damage
+        return health
 
 
 class Enemy:
@@ -318,30 +248,61 @@ class Enemy:
         self.damage = damage
 
     def draw(self):
-        fainted = False
         image = pygame.image.load(self.frontSprite)
         self.surface.blit(image, (100, 10))
 
-    def health(self):
+    def health(self, damage):
+        self.damage = damage
         health = self.hitPoints
-        health = int(health) - self.damage
+        health = int(health) - damage
+        return health
 
 
 allyDamage = 0
 enemyDamage = 0
+allyFainted = False
+enemyFainted = False
+moveList = []
+moveList2 = []
 
 allyPokemon = Ally(surface, pokeList[1], pokeList[2], pokeList[3], pokeList[4], pokeList[5], pokeList[6], pokeList[7],
                    pokeList[8], pokeList[9], pokeList[10], allyDamage)
 enemyPokemon = Enemy(surface, pokeList2[1], pokeList2[2], pokeList2[3], pokeList2[4], pokeList2[5], pokeList2[6],
                      pokeList2[7], pokeList2[8], pokeList2[9], pokeList2[11], enemyDamage)
 
-while True:
+turnCount = 0
+run = True
+while run:
     surface.fill(white)
     allyPokemon.draw()
     enemyPokemon.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            run = False
 
+    turnMove = Move(pokeList, pokeList2, moveList, moveList2)
+    moveList, moveList2 = turnMove.moveSelection(moveList, moveList2)
+    pokeDamage = turnMove.Damage(pokeList, pokeList2, moveList, moveList2)
+    pokeDamage2 = turnMove.Damage(pokeList2, pokeList, moveList2, moveList)
+    pokeList2[4] = enemyPokemon.health(pokeDamage)
+    pokeList[4] = allyPokemon.health(pokeDamage2)
+    if pokeList[4] <= 0:
+        allyFainted = True
+    else:
+        allyFainted = False
+    if pokeList2[4] <= 0:
+        enemyFainted = True
+    else:
+        enemyFainted = False
+    turnCount = turnCount + 1
+    if enemyFainted:
+        print("you win")
+        pygame.quit()
+    elif allyFainted:
+        print("you lose")
+        pygame.quit()
+
+    if not run:
+        pygame.quit()
     pygame.display.update()
